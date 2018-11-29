06655983f5b8d0a27ab39719520e967ade95cf08
Siyu <lsy19940730@163.com>
2018-11-29 23:36:04 +0800

    20181129

---
+++ src/v07_MazeGeneralization/v03_Non_Recurse_DFS_MazeSolver/Position.java
@@ -0,0 +1,14 @@
+package v07_MazeGeneralization.v03_Non_Recurse_DFS_MazeSolver;
+
+public class Position {
+
+    private int x, y;
+
+    public Position(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX(){return x;}
+    public int getY(){return y;}
+}
