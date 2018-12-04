17a7e7632953c32e9595b9e03e9d9d548cf77a55
Siyu <lsy19940730@163.com>
2018-12-05 00:09:13 +0800

    20181204 MazeGeneralization

---
+++ src/v08_RandomMazeGeneralization/v05_BFS_Maze_Generalization/Position.java
@@ -0,0 +1,14 @@
+package v08_RandomMazeGeneralization.v05_BFS_Maze_Generalization;
+
+public class Position {
+
+    private int x, y;
+
+    public Position(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX(){return x;}
+    public int getY(){return y;}
+}
