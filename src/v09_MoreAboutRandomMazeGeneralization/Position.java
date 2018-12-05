0a4710c173cc57022beec48c86e63e50d9bb229d
Siyu <lsy19940730@163.com>
2018-12-05 23:36:04 +0800

    20181205 Mine Sweeper

---
+++ src/v09_MoreAboutRandomMazeGeneralization/Position.java
@@ -0,0 +1,14 @@
+package v09_MoreAboutRandomMazeGeneralization;
+
+public class Position {
+
+    private int x, y;
+
+    public Position(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX(){return x;}
+    public int getY(){return y;}
+}
